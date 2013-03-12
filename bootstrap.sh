d072e308ccf77751c5311f793bc2b033138908d4
Luca Pucacco <luca.pucacco@xpeppers.com>
2013-03-12 10:02:40 +0100

    bootstrap.sh execution idempotent

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-dotfiles="$HOME/.dotfiles"
+dotfiles="$HOME/.dotfiles.lup"
 
 echo "\n### Installing ###"
 
 echo "\n> clone repo\n"
-git clone http://github.com/lucapuca/dotfiles.git $dotfiles
+if [ -e $dotfiles ]
+then
+    git pull origin master
+else
+    git clone http://github.com/lucapuca/dotfiles.git $dotfiles
+fi
 
 echo "> linking dotfiles\n"
+if [ -e $HOME/.gitconfig ]
+then
+    mv $HOME/.gitconfig $HOME/.gitconfig.`date +%F.%k%M%S`
+fi
 ln -s $dotfiles/gitconfig $HOME/.gitconfig
 
 echo "\n### End ###"
