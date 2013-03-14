0713d40894ade82eca93adc84185904f7546174e
Luca Pucacco <luca.pucacco@xpeppers.com>
2013-03-14 16:13:27 +0100

    added vimpeppers configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,3 +14,5 @@ ln -svfbT $dotfiles/bashrc $HOME/.bashrc
 ln -svfbT $dotfiles/gitconfig $HOME/.gitconfig
 ln -svfbT $dotfiles/git-completion.bash $HOME/.git-completion.bash
 ln -svfbT $dotfiles/git-prompt.sh $HOME/.git-prompt.sh
+
+curl https://raw.github.com/gcapizzi/vimpeppers/master/bootstrap.sh -L -o - | sh
